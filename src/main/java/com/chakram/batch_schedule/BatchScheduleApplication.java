eea7f6991159811949cc12a96b94c52d161683da
Chakrasai <chakrasai@example.com>
2025-11-01 23:29:46 +0530

    Initial commit

---
+++ src/main/java/com/chakram/batch_schedule/BatchScheduleApplication.java
@@ -0,0 +1,13 @@
+package com.chakram.batch_schedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchScheduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchScheduleApplication.class, args);
+	}
+
+}
